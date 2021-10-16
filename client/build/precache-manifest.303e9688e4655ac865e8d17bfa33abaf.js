2d5b2851e106a5e123002844ce45e20abfb76d92
Dana Bottoni <danabottoni@gmail.com>
2021-10-16 17:06:46 -0400

    build

---
+++ client/build/precache-manifest.303e9688e4655ac865e8d17bfa33abaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a94a2398fd796db04cfd829aa11f964e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "872d735b1fe28e7a6da3",
+    "url": "/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "8e3e514586c4c9f05257",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "872d735b1fe28e7a6da3",
+    "url": "/static/js/2.b9d0dbe0.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.b9d0dbe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e3e514586c4c9f05257",
+    "url": "/static/js/main.0bb1704e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
